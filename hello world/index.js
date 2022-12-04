16de8a8db4a7a89f1e9b2219ae2003d4a5a6e810
AzkaJaved <azkaishere@gmail.com>
2022-12-04 23:21:03 +0500

    practice

---
+++ hello world/index.js
@@ -0,0 +1,8 @@
+let firstName = "sajid";
+let lastName;
+lastName = "sajid";
+lastName = 7;
+console.log(firstName);
+let myName;
+myName = "j";
+export {};
---
+++ hello world/index.ts
@@ -0,0 +1,7 @@
+let firstName = "azka";
+let lastName;
+lastName = "azka";
+lastName=7;
+console.log(firstName);
+let myName : string;
+myName = "j";
